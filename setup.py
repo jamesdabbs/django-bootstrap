eac8bb355dca0ec282cee977f74afcbc9f4a3b13
James Dabbs <james.dabbs@gmail.com>
2012-07-21 09:56:38 -0400

    Corrected error in message tag handling.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-bootstrap',
-    version='0.0.1.3',
+    version='0.0.1.4',
     description='Tools for rendering templates in a format compatible with ' \
                 'Twitter Bootstrap',
 
